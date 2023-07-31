18c6bae62efd98ad3eea1aaa87d7a6451af7e1ed
Markus Essing <essing-markus@web.de>
2023-07-31 18:16:13 +0200

    fixed font error, added accelerator

--- Pages/Navigation.tsx
+++ Pages/Navigation.tsx
@@ -3,12 +3,11 @@ import { styles } from "../styles/styles";
 import {getPathstoRooms} from "../Roomfinding/Roomfinder";
 
 export const Navigation = ({ navigation, route }) => {
-    console.log(route.params.destination)
-    console.log(getPathstoRooms(route.params.destination))
   return (
     <>
       <View style={styles.container}>
-          <Text>
+          <Text>Your quickest path:</Text>
+          <Text style={{marginTop: 10, fontFamily: "Accelerator"}}>
               {getPathstoRooms(route.params.destination)}
           </Text>
       </View>
