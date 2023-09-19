f2dc76ccda3a4fcef607fe4f440651007628b5fe
Julz <julian.gkt@gmail.com>
2023-09-20 00:15:13 +0200

    Fixed Error in RoomGrid

--- components/RoomGrid.tsx
+++ components/RoomGrid.tsx
@@ -6,7 +6,7 @@ import { StyleSheet } from "react-native";
 
 
 export const RoomGrid = () => {
-    const rooms = data.buildings[0].rooms; 
+    const rooms = data.buildings[0].etage[0].rooms; 
 
     return (
         <FlatList
