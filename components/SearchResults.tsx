799acd2d55e5e909d46fbe716bb1809e63890eae
Julian Gutknecht <julian.gkt@gmail.com>
2023-10-04 10:15:14 +0200

    disabled searchresults

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,30 +1,30 @@
 import { useEffect, useState } from "react";
 import data from "../roomfinding/testData.json";
-import { View, Text, FlatList, StyleSheet, Pressable } from 'react-native';
-import { SearchBar } from './SearchBar';
+import { View, Text, FlatList, StyleSheet, Pressable } from "react-native";
+import { SearchBar } from "./SearchBar";
 import { styles } from "../styles/styles";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export const SearchResults = () => {
-    const [searchText, setSearchText] = useState('');
-    const [filteredData, setFilteredData] = useState([]);
-  
-    useEffect(() => {
-      const filtered = data.filter((item) =>
-        item.name.toLowerCase().includes(searchText.toLowerCase())
-      );
-      setFilteredData(filtered);
-    }, [searchText]);
-  
-    const handleSearch = (text) => {
-      setSearchText(text);
-    };
-  
-    return (
-      <View style={bonusStyles.container}>
-        <SearchBar onSearch={handleSearch} />
-        <SafeAreaView style={styles.roomGrid}>
-          <FlatList
+  const [searchText, setSearchText] = useState("");
+  const [filteredData, setFilteredData] = useState([]);
+
+  useEffect(() => {
+    const filtered = data.filter((item) =>
+      item.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+    setFilteredData(filtered);
+  }, [searchText]);
+
+  const handleSearch = (text) => {
+    setSearchText(text);
+  };
+
+  return (
+    <View style={bonusStyles.container}>
+      <SearchBar onSearch={handleSearch} />
+      <SafeAreaView style={styles.roomGrid}>
+        {/*<FlatList
             data={filteredData}
             keyExtractor={(item) => item.id.toString()}
             renderItem={({ item }) => (
@@ -36,21 +36,19 @@ export const SearchResults = () => {
                 <View style={[styles.roomBottomBar, {backgroundColor: "red"}]} />
               </Pressable>
             )}
-          />
-          
-        </SafeAreaView>
-      </View>
-    );
-  };
-  
-  const bonusStyles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 16,
-    },
-    item: {
-      fontSize: 16,
-      paddingVertical: 8,
-    },
+            />*/}
+      </SafeAreaView>
+    </View>
+  );
+};
+
+const bonusStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  item: {
+    fontSize: 16,
+    paddingVertical: 8,
+  },
 });
-  
