6e2d987712dd2de589f71c5f5bed91aa24583c50
Julz <julian.gkt@gmail.com>
2023-11-13 18:58:56 +0100

    pretty about page update

--- pages/SettingsPage.tsx
+++ pages/SettingsPage.tsx
@@ -6,6 +6,7 @@ import * as React from "react";
 import Logo from "../assets/Logo.svg";
 import LottieView from "lottie-react-native";
 import { useCallback, useEffect, useRef } from "react";
+import { LinearGradient } from "expo-linear-gradient";
 
 const supportedURL = 'mailto:contact@julz.life';
 
@@ -47,36 +48,35 @@ export const SettingsPage = ({ navigation}) => {
       }, [])
       
     return (
-        <View style={{backgroundColor: "#ffffff"}}>
-            <TopBar/>
-            <ScrollView contentContainerStyle={[styles.contentContainer, {paddingTop: 20,paddingBottom: 275}]}>
-                <View style={{flex: 1, justifyContent: "center", alignItems: "center", width: "100%", height: "100%"}}>
-                    <LottieView
-                        source={require('../assets/lotties/settingsHeader.json')}
-                        ref={lottieRef}
-                        loop
-                        autoPlay
-                        style={{flex: 1, height: 360}}
-                    />
-                </View>
-                <View style={[styles.settingsBox, {backgroundColor: customColors.purple, marginBottom: 40, marginTop: -60, paddingTop: 20}]}>
-                    <View style={{display: "flex", flexDirection: "row", gap: 10, justifyContent: "center"}}>
-                        <Text style={[styles.defaultHeader, {marginTop: 20.5, fontSize: 20}]}>
-                            About
-                        </Text>
-                        <Logo width={100}></Logo>
-                    </View>
-                    <Text style={[styles.defaultText, {marginTop: 10, textAlign: "center"}]}>inveni helps you with navigation on campus. Choose a room, and the app takes care of the rest.</Text>
-                    <Text style={[styles.defaultText, {marginTop: 10, textAlign: "center"}]}>The app was developed as a project by Julz and Markus, two students in Computer Visualization and Design at Hamm-Lippstadt University.</Text>
-                    <Text style={[styles.defaultText, {marginTop: 10, textAlign: "center", marginBottom: 30}]}>Do you have any questions or suggestions about the app? Write to us!</Text>
-                    <OpenURLButton url={supportedURL}>Send us an e-mail!</OpenURLButton>
-                </View>
-
-            </ScrollView>
-            <View style={{flex: 1, alignItems: "center", position: "absolute", zIndex: 3, bottom: 150, left: 0, right: 0}}>
-                <ButtonIcon size={25} buttonPadding={25} color={customColors.uwu} imageSource={require("../assets/icons/close.png")} action={() => navigation.goBack()} customStyles={styles.closeButton} />
+        <>
+            <View style={{position: "absolute", zIndex: 999, top: 60, right: 35}}>
+                <ButtonIcon size={20} buttonPadding={20} color={customColors.uwu} imageSource={require("../assets/icons/close.png")} action={() => navigation.goBack()} customStyles={styles.closeButton} />
             </View>
-
-        </View>
+            <View style={{backgroundColor: "#ffffff"}}>
+                <ScrollView contentContainerStyle={[styles.contentContainer, {paddingTop: 120, paddingBottom: 0}]}>
+                    <View style={{flex: 1, justifyContent: "center", alignItems: "center", width: "100%", height: "100%"}}>
+                        <LottieView
+                            source={require('../assets/lotties/settingsHeader.json')}
+                            ref={lottieRef}
+                            loop
+                            autoPlay
+                            style={{flex: 1, height: 360}}
+                        />
+                    </View>
+                    <View style={[styles.settingsBox, {backgroundColor: customColors.purple, marginBottom: 40, marginTop: -60, paddingTop: 20}]}>
+                        <View style={{display: "flex", flexDirection: "row", gap: 10, justifyContent: "center"}}>
+                            <Text style={[styles.defaultHeader, {marginTop: 20.5, fontSize: 20}]}>
+                                About
+                            </Text>
+                            <Logo width={100}></Logo>
+                        </View>
+                        <Text style={[styles.defaultText, {marginTop: 10, textAlign: "center"}]}>inveni helps you with navigation on campus. Choose a room, and the app takes care of the rest.</Text>
+                        <Text style={[styles.defaultText, {marginTop: 10, textAlign: "center"}]}>The app was developed as a project by Julz and Markus, two students in Computer Visualization and Design at Hamm-Lippstadt University.</Text>
+                        <Text style={[styles.defaultText, {marginTop: 10, textAlign: "center", marginBottom: 30}]}>Do you have any questions or suggestions about the app? Write to us!</Text>
+                        <OpenURLButton url={supportedURL}>Send us an Email!</OpenURLButton>
+                    </View>
+                </ScrollView>
+            </View>
+        </>
     );
 };
